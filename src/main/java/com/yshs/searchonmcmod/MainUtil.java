e24ec2d9418a045a22efd354088a1a72baa4b6f4
bling-yshs <377178599@qq.com>
2025-01-20 14:20:56 +0800

    feat: 兼容部分mod registryName不规范的问题

---
+++ src/main/java/com/yshs/searchonmcmod/MainUtil.java
@@ -0,0 +1,11 @@
+package com.yshs.searchonmcmod;
+
+import net.minecraft.Util;
+
+public class MainUtil {
+    public static final String SEARCH_PAGE_URL = "https://search.mcmod.cn/s?key=%s";
+
+    public static void openSearchPage(String name) {
+        Util.getPlatform().openUri(String.format(SEARCH_PAGE_URL, name));
+    }
+}
