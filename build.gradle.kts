d7bb835fa37a7932773c28bac31aba6713ab6111
bling-yshs <377178599@qq.com>
2026-06-14 18:46:09 +0800

    ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,13 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+tasks.register("printModVersion") {
+    group = "help"
+    description = "打印模组版本号，用于 CI/CD 工作流"
+
+    val projectVersion = project.version
+    doLast {
+        println(projectVersion)
+    }
+}
